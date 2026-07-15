8e4df85ef013636a74f5881e247c894da38c3bf2
Altay Taneri <taneri862@gmail.com>
2026-07-15 17:58:15 +0300

    chore: add Firebase SDK with Crashlytics and Analytics
    - Added firebase_core, firebase_crashlytics, firebase_analytics
    - Initialized Firebase + Crashlytics error handlers in main.dart
    - Added Android/iOS platform config (google-services, GoogleService-Info, gradle plugins)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -7,5 +12,21 @@ import 'package:saveapenny/core/config/app_environment.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   AppEnvironment.current();
+
+  await Firebase.initializeApp();
+
+  FlutterError.onError = (errorDetails) {
+    unawaited(
+      FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails),
+    );
+  };
+
+  PlatformDispatcher.instance.onError = (Object error, StackTrace stack) {
+    unawaited(
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true),
+    );
+    return true;
+  };
+
   runApp(const ProviderScope(child: App()));
 }
