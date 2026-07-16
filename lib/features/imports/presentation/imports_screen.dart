2f3ab3070b770372c7de0e3e4daab3fd00aa5d3e
Altay Taneri <taneri862@gmail.com>
2026-07-16 15:29:27 +0300

    chore: upgrade Firebase and iOS dependencies

--- lib/features/imports/presentation/imports_screen.dart
+++ lib/features/imports/presentation/imports_screen.dart
@@ -62,7 +62,7 @@ class ImportsScreen extends ConsumerWidget {
   }
 
   Future<void> _pickAndPreview(BuildContext context, WidgetRef ref) async {
-    final result = await FilePicker.platform.pickFiles(
+    final result = await FilePicker.pickFiles(
       type: FileType.custom,
       allowedExtensions: <String>['csv'],
     );
